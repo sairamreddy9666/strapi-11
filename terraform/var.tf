fdc4147a25ecb0afa714ff2e982176abb32b2251
sairamreddy9666 <115546095+sairamreddy9666@users.noreply.github.com>
2025-10-11 18:35:44 +0530

    Update var.tf

--- terraform/var.tf
+++ terraform/var.tf
@@ -7,3 +7,30 @@ variable "ecr_repository_url" {
   description = "ECR repository URL"
   type        = string
 }
+
+variable "db_host" {
+  description = "Database hostname or endpoint"
+  type        = string
+}
+
+variable "db_port" {
+  description = "Database port"
+  type        = string
+  default     = "5432"
+}
+
+variable "db_name" {
+  description = "Database name"
+  type        = string
+}
+
+variable "db_user" {
+  description = "Database username"
+  type        = string
+}
+
+variable "db_password" {
+  description = "Database password"
+  type        = string
+  sensitive   = true
+}
