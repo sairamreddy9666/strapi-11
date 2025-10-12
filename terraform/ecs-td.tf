2e2905a3bd2af0e90b9f72c0d0f154960a741737
sairamreddy9666 <115546095+sairamreddy9666@users.noreply.github.com>
2025-10-12 18:48:10 +0530

    Update ecs-td.tf

--- terraform/ecs-td.tf
+++ terraform/ecs-td.tf
@@ -4,7 +4,7 @@ resource "aws_ecs_task_definition" "TD" {
   requires_compatibilities = ["FARGATE"]
   cpu                      = 2048
   memory                   = 4096
-  execution_role_arn       = aws_iam_role.ecs_task_exec_role.arn
+  execution_role_arn       = data.aws_iam_role.ecs_task_exec_role.arn
 
   container_definitions = jsonencode([
     {
