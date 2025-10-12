276fcd00dc2a7c97d7374c1187cbdb16f8063358
sairamreddy9666 <115546095+sairamreddy9666@users.noreply.github.com>
2025-10-12 21:20:18 +0530

    Update sg.tf

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -21,3 +21,34 @@ resource "aws_security_group" "SG" {
     Name = "sairam-SG"
   }
 }
+
+resource "aws_security_group" "LB_SG" {
+  name        = "sairam-LB-SG"
+  description = "Allow HTTP and HTTPS"
+  vpc_id      = "vpc-04199d5cf351c2e08"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "sairam-LB-SG"
+  }
+}
