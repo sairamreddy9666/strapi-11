743dcf69d25bf14b6ef9473c1320dfe12bab5968
sairamreddy9666 <115546095+sairamreddy9666@users.noreply.github.com>
2025-10-12 20:08:10 +0530

    Update tg.tf

--- terraform/tg.tf
+++ terraform/tg.tf
@@ -3,9 +3,19 @@ resource "aws_lb_target_group" "TG" {
   port        = "1337"
   protocol    = "HTTP"
   target_type = "ip"
-  vpc_id      = aws_vpc.vpc.id
+  vpc_id      = "vpc-04199d5cf351c2e08"
 
   tags = {
     Name = "sairam-TG"
   }
+
+  health_check {
+    path                = "/admin"
+    matcher             = "200-399"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+
 }
