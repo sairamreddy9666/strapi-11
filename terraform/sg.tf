a76a27065870b8fdec6391a92eff0f7d4910c0e5
sairamreddy9666 <115546095+sairamreddy9666@users.noreply.github.com>
2025-10-12 20:10:23 +0530

    Update sg.tf

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -1,11 +1,11 @@
 resource "aws_security_group" "SG" {
   name        = "sairam-SG"
   description = "Allow all traffic"
-  vpc_id      = aws_vpc.vpc.id
+  vpc_id      = "vpc-04199d5cf351c2e08"
 
   ingress {
-    from_port   = 0
-    to_port     = 0
+    from_port   = 1337
+    to_port     = 1337
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
