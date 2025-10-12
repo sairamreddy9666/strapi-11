ca2a1ffce012f398427245492c71b869fa161fa7
sairamreddy9666 <115546095+sairamreddy9666@users.noreply.github.com>
2025-10-12 18:47:38 +0530

    Update iam.tf

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,15 +1,3 @@
-resource "aws_iam_role" "ecs_task_exec_role" {
+data "aws_iam_role" "ecs_task_exec_role" {
   name = "ecsTaskExecutionRole"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-        }
-      }
-    ]
-  })
 }
