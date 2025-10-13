ce02119a42ffa00250561c8bafcd5a998c883661
sairamreddy9666 <115546095+sairamreddy9666@users.noreply.github.com>
2025-10-13 22:14:09 +0530

    Update iam.tf

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -2,6 +2,24 @@ data "aws_iam_role" "ecs_task_exec_role" {
   name = "ecsTaskExecutionRole"
 }
 
-data "aws_iam_role" "CodeDeployRole" {
+resource "aws_iam_role" "CodeDeployRole" {
   name = "CodeDeployRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "codedeploy.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "CodeDeployRolePolicy" {
+  role       = aws_iam_role.CodeDeployRole.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSCodeDeployRoleForECS"
 }
