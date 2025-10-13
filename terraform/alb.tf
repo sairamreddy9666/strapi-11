023fdf336c72120031185b40bef70400745fb087
sairamreddy9666 <115546095+sairamreddy9666@users.noreply.github.com>
2025-10-13 18:24:13 +0530

    Update alb.tf

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -1,12 +1,12 @@
 resource "aws_lb" "LB" {
-  name               = "LB"
+  name               = "LB-BlueGreen"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.LB_SG.id]
   subnets            = ["subnet-01b9145f78073bb17", "subnet-03d2b77588f67313d", "subnet-0e0fe86cf5f4b935b"]
 
   tags = {
-    Name = "sairam-LB"
+    Name = "sairam-LB-BlueGreen"
   }
 }
 
@@ -16,7 +16,7 @@ resource "aws_alb_listener" "Listener" {
   protocol          = "HTTP"
 
   default_action {
-    target_group_arn = aws_lb_target_group.TG.id
+    target_group_arn = aws_lb_target_group.Blue_TG.id
     type             = "forward"
   }
 }
