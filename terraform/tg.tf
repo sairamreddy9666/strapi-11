5afbc3f3c8efd09bda3e2fbba317e3b37331d9e4
sairamreddy9666 <115546095+sairamreddy9666@users.noreply.github.com>
2025-10-13 18:28:01 +0530

    Update tg.tf

--- terraform/tg.tf
+++ terraform/tg.tf
@@ -1,21 +1,43 @@
-resource "aws_lb_target_group" "TG" {
-  name        = "TG"
+# Blue Target Group
+resource "aws_lb_target_group" "Blue_TG" {
+  name        = "Blue-TG"
   port        = "1337"
   protocol    = "HTTP"
   target_type = "ip"
   vpc_id      = "vpc-04199d5cf351c2e08"
 
   tags = {
-    Name = "sairam-TG"
+    Name = "sairam-Blue-TG"
   }
 
   health_check {
-    path                = "/admin"
+    path                = "/"
     matcher             = "200-399"
     interval            = 30
     timeout             = 5
     healthy_threshold   = 2
     unhealthy_threshold = 2
   }
+}
 
+# Green Target Group
+resource "aws_lb_target_group" "Green_TG" {
+  name     = "Green-TG"
+  port     = 1337
+  protocol = "HTTP"
+  target_type = "ip"
+  vpc_id   = "vpc-04199d5cf351c2e08"
+
+  tags = {
+    Name = "sairam-Green-TG"
+  }
+
+  health_check {
+    path                = "/"
+    matcher             = "200-399"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
 }
