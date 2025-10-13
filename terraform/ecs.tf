1803ebfb8f7373a59d17c57b3180fb7741fc00bb
sairamreddy9666 <115546095+sairamreddy9666@users.noreply.github.com>
2025-10-13 17:00:54 +0530

    Update ecs.tf

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,13 +1,6 @@
 resource "aws_ecs_cluster" "ECS" {
   name = "sairam-ECS"
 
-  capacity_providers = ["FARGATE", "FARGATE_SPOT"]
-
-  default_capacity_provider_strategy {
-    capacity_provider = "FARGATE_SPOT"
-    weight            = 1
-  }
-
   tags = {
     Name = "sairam-ECS"
   }
