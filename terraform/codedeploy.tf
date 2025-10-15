ac9975321f07431661f86028b0cae26a2b8fea2e
sairamreddy9666 <115546095+sairamreddy9666@users.noreply.github.com>
2025-10-15 19:39:07 +0530

    Update codedeploy.tf

--- terraform/codedeploy.tf
+++ terraform/codedeploy.tf
@@ -8,7 +8,7 @@ resource "aws_codedeploy_deployment_group" "ECS_DG" {
   deployment_group_name = "Strapi-ECS-DG"
   service_role_arn      = aws_iam_role.CodeDeployRole.arn
 
-  deployment_config_name = "CodeDeployDefault.ECSCanary10Percent5Minutes"
+  deployment_config_name = "CodeDeployDefault.ECSAllAtOnce"
 
   ecs_service {
     cluster_name = aws_ecs_cluster.ECS.name
@@ -24,18 +24,22 @@ resource "aws_codedeploy_deployment_group" "ECS_DG" {
     target_group_pair_info {
 
       # Blue Target Group
-      target_group {
+      prod_target_group {
         name = aws_lb_target_group.Blue_TG.name
       }
 
       # Green Target Group
-      target_group {
+      test_target_group {
         name = aws_lb_target_group.Green_TG.name
       }
 
       # Listener ARNs (must be a list)
       prod_traffic_route {
         listener_arns = [aws_alb_listener.Listener.arn]
+      }
+
+      test_traffic_route {
+        listener_arns = [aws_alb_listener.Green_Listener.arn]
       }
     }
   }
