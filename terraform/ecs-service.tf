f4c1cddbaafa435f37ef0f7820d5ab4feb852197
sairamreddy9666 <115546095+sairamreddy9666@users.noreply.github.com>
2025-10-15 20:34:07 +0530

    Update ecs-service.tf

--- terraform/ecs-service.tf
+++ terraform/ecs-service.tf
@@ -5,7 +5,7 @@ resource "aws_ecs_service" "ECS-Service" {
   cluster                            = aws_ecs_cluster.ECS.id
   task_definition                    = aws_ecs_task_definition.TD.arn
   scheduling_strategy                = "REPLICA"
-  desired_count                      = 2
+  desired_count                      = 1
   deployment_minimum_healthy_percent = 100
   deployment_maximum_percent         = 200
   force_new_deployment               = true
